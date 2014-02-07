fd29bf57184229864af9fb23590dc906e4d590ae
Clayton Smith <argilo@gmail.com>
2014-02-07 18:18:32 -0500

    Initial checkin.

---
+++ qam64.py
@@ -0,0 +1,132 @@
+#!/usr/bin/env python
+##################################################
+# Gnuradio Python Flow Graph
+# Title: Qam64
+# Generated: Fri Feb  7 18:17:49 2014
+##################################################
+
+from gnuradio import blocks
+from gnuradio import digital
+from gnuradio import eng_notation
+from gnuradio import gr
+from gnuradio.eng_option import eng_option
+from gnuradio.filter import firdes
+from gnuradio.wxgui import forms
+from grc_gnuradio import wxgui as grc_wxgui
+from optparse import OptionParser
+import numpy
+import osmosdr
+import wx
+
+class qam64(grc_wxgui.top_block_gui):
+
+    def __init__(self):
+        grc_wxgui.top_block_gui.__init__(self, title="Qam64")
+        _icon_path = "/usr/share/icons/hicolor/32x32/apps/gnuradio-grc.png"
+        self.SetIcon(wx.Icon(_icon_path, wx.BITMAP_TYPE_ANY))
+
+        ##################################################
+        # Variables
+        ##################################################
+        self.samp_rate = samp_rate = 5056941 * 2
+        self.gain = gain = 0
+        self.center_freq = center_freq = 441000000
+
+        ##################################################
+        # Blocks
+        ##################################################
+        _gain_sizer = wx.BoxSizer(wx.VERTICAL)
+        self._gain_text_box = forms.text_box(
+        	parent=self.GetWin(),
+        	sizer=_gain_sizer,
+        	value=self.gain,
+        	callback=self.set_gain,
+        	label="TX gain",
+        	converter=forms.float_converter(),
+        	proportion=0,
+        )
+        self._gain_slider = forms.slider(
+        	parent=self.GetWin(),
+        	sizer=_gain_sizer,
+        	value=self.gain,
+        	callback=self.set_gain,
+        	minimum=0,
+        	maximum=25,
+        	num_steps=25,
+        	style=wx.SL_HORIZONTAL,
+        	cast=float,
+        	proportion=1,
+        )
+        self.Add(_gain_sizer)
+        self.osmosdr_sink_0 = osmosdr.sink( args="numchan=" + str(1) + " " + "bladerf=0,buffers=128,buflen=32768" )
+        self.osmosdr_sink_0.set_sample_rate(samp_rate)
+        self.osmosdr_sink_0.set_center_freq(center_freq, 0)
+        self.osmosdr_sink_0.set_freq_corr(0, 0)
+        self.osmosdr_sink_0.set_gain(gain, 0)
+        self.osmosdr_sink_0.set_if_gain(0, 0)
+        self.osmosdr_sink_0.set_bb_gain(-4, 0)
+        self.osmosdr_sink_0.set_antenna("", 0)
+        self.osmosdr_sink_0.set_bandwidth(0, 0)
+          
+        self.digital_qam_mod_0 = digital.qam.qam_mod(
+          constellation_points=64,
+          mod_code="none",
+          differential=False,
+          samples_per_symbol=2,
+          excess_bw=0.18,
+          verbose=False,
+          log=False,
+          )
+        self.digital_map_bb_0 = digital.map_bb((36, 35, 34, 19, 20, 37, 18, 21, 28, 27, 44, 29, 26, 43, 42, 45, 52, 33, 32, 3, 22, 53, 2, 23, 30, 11, 60, 31, 10, 41, 40, 61, 38, 51, 50, 17, 4, 39, 16, 5, 12, 25, 46, 13, 24, 59, 58, 47, 54, 49, 48, 1, 6, 55, 0, 7, 14, 9, 62, 15, 8, 57, 56, 63))
+        self.blocks_unpacked_to_packed_xx_0 = blocks.unpacked_to_packed_bb(6, gr.GR_MSB_FIRST)
+        self.analog_random_source_x_0 = blocks.vector_source_b(map(int, numpy.random.randint(0, 64, 100000)), True)
+
+        ##################################################
+        # Connections
+        ##################################################
+        self.connect((self.digital_qam_mod_0, 0), (self.osmosdr_sink_0, 0))
+        self.connect((self.digital_map_bb_0, 0), (self.blocks_unpacked_to_packed_xx_0, 0))
+        self.connect((self.blocks_unpacked_to_packed_xx_0, 0), (self.digital_qam_mod_0, 0))
+        self.connect((self.analog_random_source_x_0, 0), (self.digital_map_bb_0, 0))
+
+
+# QT sink close method reimplementation
+
+    def get_samp_rate(self):
+        return self.samp_rate
+
+    def set_samp_rate(self, samp_rate):
+        self.samp_rate = samp_rate
+        self.osmosdr_sink_0.set_sample_rate(self.samp_rate)
+
+    def get_gain(self):
+        return self.gain
+
+    def set_gain(self, gain):
+        self.gain = gain
+        self.osmosdr_sink_0.set_gain(self.gain, 0)
+        self._gain_slider.set_value(self.gain)
+        self._gain_text_box.set_value(self.gain)
+
+    def get_center_freq(self):
+        return self.center_freq
+
+    def set_center_freq(self, center_freq):
+        self.center_freq = center_freq
+        self.osmosdr_sink_0.set_center_freq(self.center_freq, 0)
+
+if __name__ == '__main__':
+    import ctypes
+    import sys
+    if sys.platform.startswith('linux'):
+        try:
+            x11 = ctypes.cdll.LoadLibrary('libX11.so')
+            x11.XInitThreads()
+        except:
+            print "Warning: failed to XInitThreads()"
+    parser = OptionParser(option_class=eng_option, usage="%prog: [options]")
+    (options, args) = parser.parse_args()
+    tb = qam64()
+    tb.Start(True)
+    tb.Wait()
+
